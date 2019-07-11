f804bf6ff957111d3fd1a56b6decd43722333c3d
Damian Stasik <visualfanatic@users.noreply.github.com>
2019-07-11 16:01:57 +0200

    Setup ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  root: true,
+  parser: 'babel-eslint',
+  extends: 'airbnb',
+  env: {
+    jest: true,
+    browser: true
+  },
+
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.ts', '.tsx']
+      }
+    }
+  },
+  rules: {
+    'react/prop-types': 'off',
+    'react/jsx-filename-extension': 'off',
+    'import/prefer-default-export': 'off'
+  },
+  overrides: [
+    {
+      files: ['**/*.ts', '**/*.tsx'],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        project: './tsconfig.json'
+      },
+      plugins: ['@typescript-eslint'],
+      rules: {
+        'no-unused-vars': 'off'
+      }
+    }
+  ]
+};
