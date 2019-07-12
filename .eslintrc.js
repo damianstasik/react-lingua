7c2360de57415215f2bad19f5903bde6778e122e
Damian Stasik <visualfanatic@users.noreply.github.com>
2019-07-12 16:30:32 +0200

    Update .eslintrc.js (added missing commas + disabled object-curly-newline rule)

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,32 +4,32 @@ module.exports = {
   extends: 'airbnb',
   env: {
     jest: true,
-    browser: true
+    browser: true,
   },
-
   settings: {
     'import/resolver': {
       node: {
-        extensions: ['.js', '.ts', '.tsx']
-      }
-    }
+        extensions: ['.js', '.ts', '.tsx'],
+      },
+    },
   },
   rules: {
     'react/prop-types': 'off',
     'react/jsx-filename-extension': 'off',
-    'import/prefer-default-export': 'off'
+    'import/prefer-default-export': 'off',
   },
   overrides: [
     {
       files: ['**/*.ts', '**/*.tsx'],
       parser: '@typescript-eslint/parser',
       parserOptions: {
-        project: './tsconfig.json'
+        project: './tsconfig.json',
       },
       plugins: ['@typescript-eslint'],
       rules: {
-        'no-unused-vars': 'off'
-      }
-    }
-  ]
+        'no-unused-vars': 'off',
+        'object-curly-newline': 'off',
+      },
+    },
+  ],
 };
