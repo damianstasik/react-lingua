1be545328365269af9d3a52845ed5a823549aa21
Damian Stasik <visualfanatic@users.noreply.github.com>
2019-07-14 21:44:17 +0200

    Fix type error when passing component to cloneElement

--- src/Translation.tsx
+++ src/Translation.tsx
@@ -1,4 +1,4 @@
-import { createElement, cloneElement, Fragment, useMemo } from 'react';
+import { createElement, cloneElement, Fragment, useMemo, isValidElement } from 'react';
 import { useTranslation } from './hooks';
 
 type Props = {
@@ -13,20 +13,20 @@ export const Translation: React.FC<Props> = ({ id, values = {} }) => {
   const translation = t(id);
 
   const children = useMemo(
-    () => Object.entries(values).reduce<(JSX.Element | string)[]>(
-      (components, [k, v]) => {
+    () => Object
+      .entries(values)
+      .reduce<(JSX.Element | string)[]>((components, [k, v]) => {
         const key = `{${k}}`;
         const index = components.findIndex(component => typeof component === 'string' && component.includes(key));
 
-        if (index >= 0) {
+        if (index >= 0 && isValidElement(v)) {
           const [left, right] = (components[index] as string).split(key);
 
           components.splice(index, 1, left, cloneElement(v, { key: k }), right);
         }
 
         return components;
-      },
-    [translation]),
+      }, [translation]),
     [translation],
   );
 
